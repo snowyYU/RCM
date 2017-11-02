8df1e52b6839887978d1111f43f3e3700d7e8de5
snowyyu <731444920@qq.com>
2017-11-02 20:05:41 +0800

    add modify password component

--- src/app/modifyPassword/modifyPassword.service.ts
+++ src/app/modifyPassword/modifyPassword.service.ts
@@ -11,23 +11,31 @@ export class ModifyPasswordService{
   setPassword(body:{
     employeeId:string,
     pwd:string,
+    oldPwd:string,
     type:number|string
-  }):Promise<{ok:boolean,message:string}>{
+  }):Promise<any>{
     return this.myHttp.post({
       url:API.loginHost+API.setPassword.url,
       body:body
     }).toPromise()
-      .then((res)=>{
-        let data={
-          ok:false,
-          message:''
-        };
-        if(res.ok){
-          let response=res;
-          data.ok=(response.status==200);
-          data.message=response.message;
-        }
-        return Promise.resolve(data);
-      });
+    .then((res)=>{
+      if (res.status==200) {
+        return Promise.resolve(res)
+      }else{
+        return Promise.reject(res)
+      }
+
+
+      // let data={
+      //   ok:false,
+      //   message:''
+      // };
+      // if(res.ok){
+      //   let response=res;
+      //   data.ok=(response.status==200);
+      //   data.message=response.message;
+      // }
+      // return Promise.resolve(data);
+    });
   }
 }
