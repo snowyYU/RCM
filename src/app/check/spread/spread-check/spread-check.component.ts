6dfdc9ffc1f4668ed4126344305030c85dea9731
wittyMouse <505357658@qq.com>
2017-11-30 18:13:03 +0800

    修改展期页面

--- src/app/check/spread/spread-check/spread-check.component.ts
+++ src/app/check/spread/spread-check/spread-check.component.ts
@@ -179,63 +179,95 @@ import { SubmitLoadingService } from '../../../../utils/submit-loading/submit-lo
 })
 export class SpreadCheckComponent implements OnInit{
     loading:boolean
+    submitting:boolean=false
+    down:boolean=true
+    show:boolean=true
 
-    rolloverApplyId              //展期单号
-    borrowApplyId                //贷款单号 
-    memberId                     //会员ID
-    repaymentPrinciple           //展期金额(元)
-    companyName                  //会员名称
-    status                       //状态
-    statusName                   //状态,中文
-    createTime                   //申请日期
-    remarks                      //申请原因
-    auditOneTime	             //审批时间
-    auditOneBy	                 //审核员
-    auditOneRemarks              //审核意见
-    repaymentPlan	             //还款期数
-    rolloverDate	             //申请承诺还款日期
-    comfirmRolloverDate	         //批准还款日期
-    realRolloverDate	         //实际还款日期
-    rolloverRate	             //展期利率
-    paymentWayName
     extendRate
-    auditTwoTime	             //审批时间
-    auditTwoBy	                 //审核员
-    auditTwoRemarks              //审核意见
-    
 
-    rolloverData:{               //展期数据
+    rolloverApplyId            //展期单号
+    createTime                //创建时间
+    status                    //状态
+    statusName                //状态，中文
+    
+    memberId                //会员ID
+    companyName                //企业名称
+    repaymentCapital          //展期金额
+    rolloverDeposit            //展期保证金
+    repaymentInterest        //本期利息
+    rolloverRate            //展期利率，百分比
+    rolloverDate            //承诺还款日期
+    comfirmRolloverDate        //批准还款日期
+
+    remarks                    //申请理由
+    
+    borrowApplyId            //贷款单号
+    currentPeriod            //还款期数
+    currentPeriodStr        //还款期数，中文
+    
+    fileLoadId                //文件提取码
+    
+    auditOneBy                //一审人
+    auditOneRemarks            //一审意见
+    auditOneTime            //一审时间
+ 
+    auditTwoTime            //二审时间
+    auditTwoBy                //二审员
+    auditTwoRemarks         //二审意见
+
+    approveAmount            //贷款金额
+    productId                //产品ID
+    productName                //产品名称
+    ratedCycle                //贷款周期
+    paymentWay                //还款方式
+    paymentWayDic            //还款方式，中文
+    rate                    //利率
+    rateType                //计息方式
+    rateTypeDic                //计息方式，中文
+    
+    repaymentDate            //到期还款日
+
+    rolloverData:{          //展期数据
         rolloverApplyId?
         createTime?
+        status?
         statusName?
         memberId?
         companyName?
-        resourceId?
+        repaymentCapital?
+        rolloverDeposit?
+        repaymentInterest?
+        rolloverRate?
+        rolloverDate?
+        comfirmRolloverDate?
         remarks?
         borrowApplyId?
-        repaymentPlan?
+        currentPeriod?
+        currentPeriodStr?
+        fileLoadId?
+        auditOneBy?
         auditOneRemarks?
+        auditOneTime?
+        auditTwoTime?
+        auditTwoeBy?
+        auditTwoRemarks?
     }={}     
                               
-    financeData:{                //借款单数据
+    financeData:{          //借款单数据
         borrowApplyId?
         approveAmount?
-        productName?
-        borrowHowlong?
-        resourceId?
         productId?
-        repaymentWay?
+        productName?
+        ratedCycle?
+        paymentWay?
+        paymentWayDic?
+        rate?
+        rateType?
+        rateTypeDic?
     }={}
 
-    productData:{                //产品参数数据
-        paymentWayName?
-        interestValue?
-        interestTypeName?
-    }={}
+    repaymentList:any[]     //还款计划数据
 
-    repaymentList:any[]          //还款计划数据
-
-    submitting: boolean = false
     constructor(
 		private router:Router,
 		private route:ActivatedRoute,
@@ -255,8 +287,8 @@ export class SpreadCheckComponent implements OnInit{
         this.spreadCheck.getRolloverDetail(id)
         .then(res=>{
             console.log(res)
-            this.rolloverData=res.body.records[0]
-            return Promise.resolve(res.body.records[0])
+            this.rolloverData=res.body
+            return Promise.resolve(res.body)
         })
         .then(res=>{
             this.spreadCheck.getfinanceApply(res.borrowApplyId)
@@ -266,25 +298,18 @@ export class SpreadCheckComponent implements OnInit{
                 return Promise.resolve(res.body.records[0])
             })
             .then(res=>{
-                this.spreadCheck.getProductsAttach(res.resourceId,res.productId,res.repaymentWay)
+                this.spreadCheck.getRepaymentPlan(res.borrowApplyId)
                 .then(res=>{
                     console.log(res)
-                    this.productData=res.body.records[0]
-                    return Promise.resolve(res.body.records[0])
+                    this.repaymentList=res.body.records
                 })
-                .then(res=>{
-                    this.spreadCheck.getRepaymentPlan(this.financeData.borrowApplyId)
-                    .then(res=>{
-                        console.log(res)
-                        this.repaymentList=res.body.records
+                .catch(res=>{
+                    this.pop.error({
+                        title:'错误信息',
+                        text:res.message
                     })
-                    .catch(res=>{
-                        this.pop.error({
-                                title:'错误信息',
-                            text:res.message
-                        })
-                    })  
-                })
+                })  
+
             })
         })
     }
@@ -330,6 +355,16 @@ export class SpreadCheckComponent implements OnInit{
             })
             this.submitting=false
         })
+    }
+
+    downEvent(){
+        this.show=true;
+        setTimeout(()=>{this.down=true},300);
+    }
+
+    upEvent(){
+        this.show=false;
+        setTimeout(()=>{this.down=false},300);
     }
 
     back(){
